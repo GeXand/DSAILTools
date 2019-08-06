0f553b8a26cae15731e6bba18f731a583cb8eda7
Xander Ge <xander.q.ge@gmail.com>
2019-08-06 12:14:48 -0500

    Began Adding "Utils" for commonly used function across all scrapers

---
+++ scrapers/scrapeUtils.py
@@ -0,0 +1,25 @@
+import xlsxwriter
+from pathlib import Path
+
+#Gets a list of links from a list of Tag objects
+def links_from_tags(linkTags):
+    links = []
+    for linkTag in linkTags:
+        link = linkTag['href']
+        links.append(link)
+    return links
+
+def createDSAILsheet(name: str):
+    # Create an excel file to store the posts
+    path = Path("C:/Users/xande/Documents/DSAIL")
+    postsBook = xlsxwriter.Workbook(path / name + ".xlsx")
+    postsSheet = postsBook.add_worksheet()
+
+    cell_format = postsBook.add_format({"text_wrap": True})
+    postsSheet.set_column(2, 2, 80, cell_format)
+
+    postsSheet.write(0, 0, "Primary")
+    postsSheet.write(0, 1, "Secondary")
+    postsSheet.write(0, 2, "Reviews")
+
+    return postsBook
