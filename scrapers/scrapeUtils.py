9deb1b72fb15ec463f8e69661af1b3fe3654c7ec
Xander Ge <xander.q.ge@gmail.com>
2019-08-08 15:51:14 -0500

    Added writing lists of split posts to sheet

--- scrapers/scrapeUtils.py
+++ scrapers/scrapeUtils.py
@@ -2,7 +2,7 @@ import xlsxwriter
 from pathlib import Path
 
 #Gets a list of links from a list of Tag objects
-def links_from_tags(linkTags):
+def linksFromTags(linkTags):
     links = []
     for linkTag in linkTags:
         link = linkTag['href']
@@ -12,7 +12,7 @@ def links_from_tags(linkTags):
 def createDSAILsheet(name: str):
     # Create an excel file to store the posts
     path = Path("C:/Users/xande/Documents/DSAIL")
-    postsBook = xlsxwriter.Workbook(path / name + ".xlsx")
+    postsBook = xlsxwriter.Workbook(path / name)
     postsSheet = postsBook.add_worksheet()
 
     cell_format = postsBook.add_format({"text_wrap": True})
@@ -22,7 +22,17 @@ def createDSAILsheet(name: str):
     postsSheet.write(0, 1, "Secondary")
     postsSheet.write(0, 2, "Reviews")
 
-    return postsBook
+    return postsBook, postsSheet
+
+def postListToSheet(l: list, sheet: xlsxwriter.Workbook.worksheet_class):
+    col = 2
+    row = 1
+    for post in l:
+        for sentence in post:
+            sheet.write(row, col, sentence)
+            row += 1
+
+    print("Sentences scraped: " + str(row))
 
 def printList(l: list):
     print("/n".join(map(str, l)))
